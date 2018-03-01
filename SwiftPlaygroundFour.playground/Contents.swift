249d27c677b23506947f6e00e341ad0f14fd7dce
tafitzs <tafitzs@hawaii.edu>
2018-02-28 21:39:59 -1000

    Complete SwiftPlaygroundFour

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+
+//for number in 1...5
+//{
+   // print(number)
+//}
+
+var sum = 0
+for number in 1...5
+{
+    sum += number
+    print(sum)
+}
+
+var i = 1
+while i <= 10
+{
+    //we want to print i
+    print(i)
+    //we want to add +1 to i for each iteration through the loop
+    i = i + 1
+}
+
